1314f22dd6c2d0e5eb03104904c419a7633509e5
Shubham Sihasane <78297361+shubham-sihasane@users.noreply.github.com>
2025-01-28 12:11:01 +0530

    Update V4-EC2-With_VPC_for_each.tf

--- terraform_code/V4-EC2-With_VPC_for_each.tf
+++ terraform_code/V4-EC2-With_VPC_for_each.tf
@@ -3,9 +3,9 @@ provider "aws" {
 }
 
 resource "aws_instance" "demo-server" {
-    ami = "ami-053b0d53c279acc90"
+    ami = "ami-0ac4dfaf1c5c0cce9"
     instance_type = "t2.micro"
-    key_name = "dpp"
+    key_name = "webserver"
     //security_groups = [ "demo-sg" ]
     vpc_security_group_ids = [aws_security_group.demo-sg.id]
     subnet_id = aws_subnet.dpp-public-subnet-01.id 
